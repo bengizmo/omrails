87b62d65c7101b7e10ddc7595f2e36dd524c8abf
Benjamin Reed <benreed1987@gmail.com>
2013-04-01 03:02:10 -0600

    Add Boostrap

---
+++ test/functional/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get force" do
+    get :force
+    assert_response :success
+  end
+
+end

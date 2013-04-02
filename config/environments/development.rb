8ba46af76bfe7327def5c9419b6df3236e5202fa
Benjamin Reed <benreed1987@gmail.com>
2013-04-01 18:53:49 -0600

    Add Paperclip Gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,5 +37,6 @@ Omrails::Application.configure do
 
   #  In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  Paperclip.options[:command_path] = 'B:/Programs/ImageMagick-6.8.4-Q16/'
 
 end
